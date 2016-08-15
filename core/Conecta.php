91781ef1f8fe01773c2e4afbf134dbccb272883a
Angelo <angeloravello26@gmail.com>
2016-08-15 03:56:23 -0500

    funcionando

--- core/Conecta.php
+++ core/Conecta.php
@@ -8,57 +8,78 @@ class Conecta {
     private $conexion;
     private $resultado;
     private $sql='SELECT c.nomCli, s.feInicio, s.fechaFin, c.pago, s.estado FROM serpag sp join servicio s on s.idmen = sp.idmen join pago p on p.idPago = sp.idpago join cliente c on c.idCli = p.idCli ';
+    
     public function __construct() {
         
     }
-    public function RealizarConexion(){
-        $this->conexion=  mysqli_connect($this->host,$this->user,$this->password,$this->database_name)or die('no se pudo conectar');
-        
-    }
+    
     public function Proceso(){
-        $this->RealizarConexion();
-        mysqli_select_db($this->conexion, $this->database_name)or die('no se encuentra bd');
-        $this->result();
+        $this->conexion= mysqli_connect($this->getHost(), $this->getUser(), $this->getPassword(), $this->getDatabase_name() ) or die ('mal conectado');
+        $this->resultado = mysqli_query($this->conexion, $this->sql)or die ('no hay resultado');
     }
-    
-    public function result(){
-        $this->resultado=  mysqli_query($this->conexion,$this->sql)or die('no hay resultados');
-    }
-
-    public function EjecutarSql(){
-        $this->setPeticion(mysqli_query($this->getConexion(),$this->getQuery())or die(('consulta fallida')));
-
-    }
-    
+        
     public function CerrarConexion(){
         mysqli_close($this->getConexion());
     }
-function getConexion() {
-    return $this->conexion;
+    
+    function getHost() {
+        return $this->host;
+    }
+
+    function getUser() {
+        return $this->user;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function getDatabase_name() {
+        return $this->database_name;
+    }
+
+    function getConexion() {
+        return $this->conexion;
+    }
+
+    function getResultado() {
+        return $this->resultado;
+    }
+
+    function getSql() {
+        return $this->sql;
+    }
+
+    function setHost($host) {
+        $this->host = $host;
+    }
+
+    function setUser($user) {
+        $this->user = $user;
+    }
+
+    function setPassword($password) {
+        $this->password = $password;
+    }
+
+    function setDatabase_name($database_name) {
+        $this->database_name = $database_name;
+    }
+
+    function setConexion($conexion) {
+        $this->conexion = $conexion;
+    }
+
+    function setResultado($resultado) {
+        $this->resultado = $resultado;
+    }
+
+    function setSql($sql) {
+        $this->sql = $sql;
+    }
+
+
+
+
 }
-
- function getResultado() {
-    return $this->resultado;
-}
-
- function getSql() {
-    return $this->sql;
-}
-
- function setConexion($conexion) {
-    $this->conexion = $conexion;
-}
-
- function setResultado($resultado) {
-    $this->resultado = $resultado;
-}
-
- function setSql($sql) {
-    $this->sql = $sql;
-}
-}
-
-
-
-
 ?>
